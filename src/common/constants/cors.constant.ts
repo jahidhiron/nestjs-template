5df35f9ccc6cacde97d11f0cc638c6df0c097aaf
Jahid Hiron <namehiron.96@gmail.com>
2025-12-24 12:20:29 +0600

    issue: cors edjustment

--- src/common/constants/cors.constant.ts
+++ src/common/constants/cors.constant.ts
@@ -1,6 +1,4 @@
-import { API_BASE_URL } from 'src/common/constants/app.constant';
-
 export const ALLOW_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
-export const ALLOWED_ORIGINS = [API_BASE_URL];
+export const ALLOWED_ORIGINS = [];
 // example: ['.your-domain']
 export const ALLOWED_DOMAINS = [];
